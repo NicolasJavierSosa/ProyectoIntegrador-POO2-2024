39eaf89ab9c61ba4ead9e2036a3695334cb3032d
BiancaMichelle <michellebianca.2002@gmail.com>
2024-11-23 18:51:57 -0300

    cambios

---
+++ proyecto/proyecto/src/main/java/com/integrador/proyecto/ProyectoIntegradorPoo2Application.java
@@ -0,0 +1,34 @@
+package com.integrador.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * Clase principal para la aplicación Spring Boot del Proyecto.
+ * 
+ * <p>
+ * Esta clase inicia la aplicación excluyendo la configuración automática de la fuente
+ * de datos, lo cual es útil si no se necesita conexión a una base de datos o si
+ * se desea configurar manualmente(lo cual nos es útil para el comienzo del proyecto).
+ * </p>
+ * 
+ * <p>Para ejecutar la aplicación se utiliza el método {@link #main(String[])} </p>
+ * 
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ProyectoIntegradorPoo2Application {
+/**
+ * Método principal de la aplicación que lanza la instancia de Spring Boot.
+ * 
+ * </P>
+ * Utiliza {@link SpringApplicarion#run(Class, String...)} para iniciar el
+ * contexto de Spring y, como es una aplicación web, también el servidor embebido.
+ * </p>
+ * @param args Argumentos de línea de comandos para personalizar el arranque de la aplicación
+ */
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradorPoo2Application.class, args);
+	}
+
+}
