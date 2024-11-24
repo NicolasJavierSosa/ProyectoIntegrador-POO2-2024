cedbb40dd28a8bab6ead36659ac42c08917f5e98
BiancaMichelle <michellebianca.2002@gmail.com>
2024-11-23 21:10:23 -0300

    update

---
+++ proyecto/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.integrador.proyecto"},{"l":"com.integrador.proyecto.controller"},{"l":"modelo"}];updateSearchResults();
