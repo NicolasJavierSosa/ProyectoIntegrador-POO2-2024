cb136ac2e34d0cba999e8458b4fe7c048a86e08b
BiancaMichelle <127901158+BiancaMichelle@users.noreply.github.com>
2024-11-25 23:14:41 -0300

    Update OrdenController.java

--- proyecto/src/main/java/com/integrador/proyecto/controller/OrdenController.java
+++ proyecto/src/main/java/com/integrador/proyecto/controller/OrdenController.java
@@ -1,17 +1,33 @@
 package com.integrador.proyecto.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.integrador.proyecto.servicio.ProductoServicio;
 
 import lombok.NoArgsConstructor;
+import modelo.Producto;
 
 @Controller
 @NoArgsConstructor
 public class OrdenController {
 
-    @GetMapping("/orden")
-    public String inicio(){
-        return "orden";
+    @Autowired
+    private ProductoServicio productoServicio;
+
+   @GetMapping("/orden/{id}")
+    public String mostrarOrden(@PathVariable String id, Model model) {
+        // Obtener la información del producto por su ID
+        Producto producto = productoServicio.obtenerProductoPorId(id);
+
+        // Agregar la información del producto al modelo
+        model.addAttribute("producto", producto);
+
+        // Redirigir al archivo HTML de la orden
+        return "orden"; // Nombre del archivo orden.html en la carpeta templates
     }
 
 }
