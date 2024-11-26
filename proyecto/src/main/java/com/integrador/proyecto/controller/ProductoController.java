37d70fa42bdb0996277316e7ea9f5d7aa4fc0121
BiancaMichelle <127901158+BiancaMichelle@users.noreply.github.com>
2024-11-25 23:14:57 -0300

    Update ProductoController.java

--- proyecto/src/main/java/com/integrador/proyecto/controller/ProductoController.java
+++ proyecto/src/main/java/com/integrador/proyecto/controller/ProductoController.java
@@ -1,15 +1,41 @@
 package com.integrador.proyecto.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.NoArgsConstructor;
+import com.integrador.proyecto.servicio.ProductoServicio;
+
+import modelo.Producto;
 
 @Controller
-@NoArgsConstructor
+@RequestMapping("/productos")
 public class ProductoController {
-    @GetMapping("/producto")
-    public String inicio(){
-        return "producto";
+    
+    @Autowired
+    private final ProductoServicio productoService;
+
+    public ProductoController(ProductoServicio productoService) {
+        this.productoService = productoService;
+    }
+
+    @GetMapping("/producto/{id}")
+    public String mostrarProducto(@PathVariable String id, Model model) {
+        Producto producto = productoService.obtenerProductoPorId(id);
+        if (producto != null) {
+            model.addAttribute("producto", producto);
+            return "producto"; // Nombre de la plantilla HTML (producto.html)
+        } else {
+            return "error"; // Redirigir a una página de error si el producto no existe
+        }
+    }
+
+    @GetMapping("/catalogo")
+    public String mostrarCatalogo(Model model) {
+        model.addAttribute("productos", productoService.obtenerProductos());
+        return "catalogo";
     }
 }
