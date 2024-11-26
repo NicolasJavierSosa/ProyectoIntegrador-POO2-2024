02e131bcb6e58158cd025b110224e4986fa980c7
BiancaMichelle <127901158+BiancaMichelle@users.noreply.github.com>
2024-11-25 23:13:12 -0300

    Update CarritoController.java

--- proyecto/src/main/java/com/integrador/proyecto/controller/CarritoController.java
+++ proyecto/src/main/java/com/integrador/proyecto/controller/CarritoController.java
@@ -17,13 +17,6 @@ public class CarritoController {
         return "carrito";
     }
 
-    /*@GetMapping("/carrito")
-    public String mostrarProductos(Model model){
-        List<Producto> productos = new ArrayList<>();
-		productos.add(new Producto("Lechuga", "Es una verdura man media pila", 100.50, 0.45));
-        productos.add(new Producto("Tomate", "Es una fruta man media pila", 1070.50, 0.50));
-        model.addAttribute("productos", productos);
-        return "carrito";
-    }*/
 
 }
+
